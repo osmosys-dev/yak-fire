3a81accb0490663bd3da48fab76a8d4bb0a3cd54
Evan Pierre Louis <evan.pierrelouis.github@gmail.com>
2020-11-04 19:54:46 +1100

    Implemented manual and auto dark mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -11,22 +12,38 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
 
+  public manualMode = true;
+  public systemAutoMode: boolean;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private renderer: Renderer2,
+    @Inject(DOCUMENT) private document: Document,
   ) {
     this.initializeApp();
   }
 
+  ngOnInit() {
+    this.renderer.setAttribute(this.document.body, 'class', 'dark');
+    this.checkColorSchemeSystemWide();
+  }
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
     });
   }
-
-  ngOnInit() {
-
+  checkColorSchemeSystemWide(){
+    const colorScheme = window.matchMedia('(prefers-color-scheme: dark)');
+    colorScheme.addEventListener('change', mediaQuery => {
+      this.systemAutoMode = mediaQuery.matches;
+      this.document.body.classList.toggle('dark', this.systemAutoMode);
+    });
+  }
+  toggleTheme(){
+    this.manualMode = !this.manualMode;
+    this.document.body.classList.toggle('dark', this.manualMode);
   }
 }
