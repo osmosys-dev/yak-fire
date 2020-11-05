b77cea297fc6953c46e5eaefa7a1935d40745cc8
Evan Pierre Louis <evan.pierrelouis.github@gmail.com>
2020-11-05 11:54:53 +1100

    Implemented theme service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,10 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { Plugins } from '@capacitor/core';
-import { DOCUMENT } from '@angular/common';
+import { ThemeService } from './services/theme.service';
 
-const {Storage} = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -21,8 +19,9 @@ export class AppComponent implements OnInit {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    @Inject(DOCUMENT) private document: Document,
+    private themeService: ThemeService,
   ) {
+
     this.initializeApp();
   }
 
@@ -34,35 +33,12 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.checkColorSchemeSystemWide();
-    this.getDarkModeValue().then((value) => {
-      this.document.body.classList.toggle('dark', value);
-    });
-  }
+    this.themeService.getTheme();
+    this.themeService.checkColorSchemeSystemWide(this.isDarkMode);
 
-  checkColorSchemeSystemWide(){
-    const colorScheme = window.matchMedia('(prefers-color-scheme: dark)');
-    colorScheme.addEventListener('change', mediaQuery => {
-      this.isDarkMode = mediaQuery.matches;
-      this.setDarkModeValue(this.isDarkMode);
-    });
   }
-
   toggleTheme(){
     this.isDarkMode = !this.isDarkMode;
-    this.setDarkModeValue(this.isDarkMode);
-  }
-
-  async setDarkModeValue(mode: boolean){
-    await Storage.set({
-      key: 'isDarkMode',
-      value: JSON.stringify(mode)
-    });
-    this.document.body.classList.toggle('dark', mode);
-  }
-
-  async getDarkModeValue(): Promise<boolean>{
-    const returnedValue = await Storage.get({key: 'isDarkMode'});
-    return JSON.parse(returnedValue.value);
+    this.themeService.setTheme(this.isDarkMode);
   }
 }
